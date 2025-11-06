b2c366dca69706a0c14226e52ee41e108834998f
cofls-dev <cofls@sookmyung.ac.kr>
2025-11-06 16:02:24 +0900

    06

--- main.c
+++ main.c
@@ -8,20 +8,33 @@ int main(int argc, char *argv[]) {
 	
 	char input[100];
 	FILE* fp;
-	int i;
+	char c;
 	
 	//fopen
-	fp = fopen("sample.txt", "w");
-	
-	for (i=0;i<3;i++)
-	
+	fp = fopen("sample.txt", "r");
+	if (fp == NULL)
 	{
+		printf("Failed to open file!\n");
+		return -1;
+	}
 	
-	//fprintf
-	printf("input a word:");
-	scanf("%s", input);
-	fprintf (fp, "%s\n", input);
-}
+	#if 0
+	while ( (c = fgetc(fp)) != EOF)
+	{
+		putchar(c);
+		
+	}
+	
+	#else
+	while ( fgets(input, 100, fp)  > 0 )
+	{
+		printf("%s", input);
+	}
+	#endif
+	
+	
+	
+	
 	//fclose
 	fclose(fp);
 	
